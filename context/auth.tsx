46cba3ed127a8d35fa9134cf55b91965f08719ad
taka-yu-ki <taka7_zegs@icloud.com>
2023-05-27 10:29:56 +0900

    ユーザーデータの取得 20230527

--- context/auth.tsx
+++ context/auth.tsx
@@ -1,4 +1,8 @@
-import { onAuthStateChanged } from "firebase/auth";
+import {
+  User as FirebaseUser,
+  onAuthStateChanged,
+  Unsubscribe,
+} from "firebase/auth";
 import {
   ReactNode,
   createContext,
@@ -6,38 +10,61 @@ import {
   useEffect,
   useState,
 } from "react";
-import { auth } from "../firebase/client";
+import { auth, db } from "../firebase/client";
+import { doc, onSnapshot } from "firebase/firestore";
+import { User } from "../types/user";
 
 type ContextType = {
-  isLoggedIn: boolean;
+  fbUser: FirebaseUser | null | undefined;
   isLoading: boolean;
+  user: User | null | undefined;
 };
 
 // ログイン情報を送る荷物
 const AuthContext = createContext<ContextType>({
-  isLoggedIn: false,
+  fbUser: undefined,
   isLoading: true,
+  user: undefined,
 });
 
 // 配送エリアを指定するコンポーネント
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   // この状態が変わるごとに再配送される
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState<User | null>();
+  const [fbUser, setFbUser] = useState<FirebaseUser | null>();
   const [isLoading, setIsLoading] = useState(true);
 
   // useEffectの第２引数を空にすることでAuthProviderが呼ばれたら、最初の１回目だけ走る
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
-      setIsLoggedIn(!!user);
-      setIsLoading(false);
+    // onメソッドでの監視員を外す変数
+    let unsubribe: Unsubscribe;
+
+    onAuthStateChanged(auth, (resultUser) => {
+      unsubribe?.();
+      setFbUser(resultUser);
+      setIsLoading(true);
+
+      if (resultUser) {
+        setIsLoading(true);
+        const ref = doc(db, `users/${resultUser.uid}`);
+        unsubribe = onSnapshot(ref, (snap) => {
+          // asを使用するのは好ましくないが、説明上asで型を固定している
+          setUser(snap.data() as User);
+          setIsLoading(false);
+        });
+      } else {
+        setUser(null);
+        setIsLoading(false);
+      }
     });
   }, []);
 
   return (
     <AuthContext.Provider
       value={{
-        isLoggedIn,
+        fbUser,
         isLoading,
+        user,
       }}
     >
       {children}
