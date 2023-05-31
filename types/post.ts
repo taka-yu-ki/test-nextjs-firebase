d76142c0bd4b4cf1ee4457bbbd278ddfe1101f2f
taka-yu-ki <taka7_zegs@icloud.com>
2023-05-31 16:13:36 +0900

    検索画面の作成 20230531

--- types/post.ts
+++ types/post.ts
@@ -1,8 +1,10 @@
+import { Timestamp } from "firebase-admin/firestore";
+
 export type Post = {
   id: string;
   title: string;
   body: string;
   createdAt: number;
   updatedAt: number | null;
-  authorId: string | undefined;
+  authorId: string;
 };
