fabc509d3a7a83afaaa5f19c7c6ed7288dda5611
taka-yu-ki <taka7_zegs@icloud.com>
2023-06-14 08:48:02 +0900

    画像のアップロード 20230614

--- components/button.tsx
+++ components/button.tsx
@@ -8,7 +8,7 @@ const Button = ({
 }) => {
   return (
     <button
-      className="px-4 py-2 rounded-full bg-blue-500 text-white shadow"
+      className="px-4 py-2 rounded-full bg-blue-500 text-white shadow disabled:opacity-30"
       {...props}
     >
       {children}
